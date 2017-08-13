e821e43383e18dca4f0386649afa38e84689a666
zyr <1041953637@qq.com>
2017-08-13 13:47:07 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello Loli World!");
+		System.out.println("I like Loli World!");
 	}
 }
